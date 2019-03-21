276d51a6af7e6342e007d93ca15c27267542055c
RickBell <richard.belamy123@gmail.com>
2019-03-20 20:41:16 -0700

    trying on heroku to serve html4

--- index.js
+++ index.js
@@ -3,13 +3,10 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-
 app.use(express.static('./public'));
-
 app.get('/', (request, response) => {
   response.render('public/index.html');
 });
-
 app.listen(PORT, () => {
   console.log('Web Server up on port', PORT);
 });
