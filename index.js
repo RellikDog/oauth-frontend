baf07dcbd754d3d9966eb4a201a3fa1ead543125
RickBell <richard.belamy123@gmail.com>
2019-03-20 20:48:38 -0700

    ejs?

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 app.use(express.static('./public'));
+app.set('view engine', 'ejs');
 app.get('/', (request, response) => {
-  response.render('public/index.html');
+  response.render('public/index.ejs');
 });
 app.listen(PORT, () => {
   console.log('Web Server up on port', PORT);
